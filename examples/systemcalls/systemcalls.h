93595c82b4aae38507c69574758e0d92cdbce43f
ajsanthosh14 <ajsanthosh12@gmail.com>
2022-01-27 23:02:07 -0700

    Part-1 complete

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -2,6 +2,15 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 bool do_system(const char *command);
 
 bool do_exec(int count, ...);
